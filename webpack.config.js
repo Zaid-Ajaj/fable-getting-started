cc80de3140374ae29df6321a8a5cb52103ec0a8c
Zaid <zaid.naom@gmail.com>
2019-09-15 14:40:17 +0200

    Simplify webpack config and update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,11 @@
-// Note this only includes basic configuration for development mode.
-// For a more comprehensive configuration check:
-// https://github.com/fable-compiler/webpack-config-template
-
 var path = require("path");
 
-// If we're running the webpack-dev-server, assume we're in development mode
-var isProduction = !process.argv.find(v => v.indexOf('webpack-dev-server') !== -1);
-console.log('Bundling for ' + (isProduction ? 'production' : 'development') + '...');
-
 module.exports = {
-    mode: "development",
+    mode: "none",
     entry: "./src/App.fsproj",
-    output: {
-        path: path.join(__dirname, "./public"),
-        filename: "bundle.js",
-    },
     devServer: {
-        contentBase: "./public",
-        port: 8080,
+        contentBase: path.join(__dirname, "./dist")
     },
-    devtool: isProduction ? 'source-map' : 'eval-source-map',
     module: {
         rules: [{
             test: /\.fs(x|proj)?$/,
